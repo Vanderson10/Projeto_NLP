6ad9f30258715f9c36d4aa949d10bc45f581199e
Vanderson10 <araujovanderson223@gmail.com>
2023-10-25 00:07:39 -0300

    Base back e front

--- backend/app.py
+++ backend/app.py
@@ -1,39 +1,47 @@
 from flask import Flask, request, jsonify
+from prompt import prompt
 import openai
 import config
+from flask_cors import CORS
 
-# Configure a chave da API do OpenAI
+
 openai.api_key = config.OPENAI_API_KEY
 
 app = Flask(__name__)
+CORS(app)
 
-# Função para criar uma conversa interativa
-def criar_conversa(redacao, modelo):
-    conversa = [
-        {"role": "system", "content": "Você é um assistente de linguagem que irá corrigir a redação."},
-        {"role": "user", "content": redacao}
+# texto = f"""
+# ‘’’Tema: VALORIZAÇÃO DO IDOSO
+
+# É inegável que a evolução da ciência e da tecnologia contribuiu para a criação de novas formas de combate à mortalidade e provocou um aumento significativo da expectativa de vida. Porém a imposição de padrões rígidos de classificação do “velho” e do “novo”, as relações de poder impressas pela sociedade de consumo e um sistema educacional desvinculado das questões essenciais da formação humana levam, respectivamente, à incompreensão do envelhecimento natural e à coi-
+# sificação e descarte dos idosos.
+# Segundo pesquisa realizada pela Sociedade Internacional de Cirurgias Plásticas Estéticas, mostra-se crescente o número de indivíduos, em todo o mundo, que, insatisfeitos com a aparência não-juvenil, procuram clínicas de estética para reverterem tal situação. No Brasil, o número supera um milhão de casos, o que consome a 13 % do total mundial. Essa procura parece se dá pelo fato de muitos não terem sido educados, desde a infância, de forma a compreenderem o processo natural de envelhecimento e, consequentemente, não desenvolverem o respeito às diferenças atuais e futuras, o que causa aversão a qualquer desvio dos padrões impostos.
+# É importante destacar, também, que a sociedade de consumo mostra como principal criadora de estereótipos, nos quais a jovialidade está ligada ao consumo modern. Nesse papel de estereotipação, utiliza-se o poderoso instrumento para se instaurar modelos de beleza, que estão relacionados, de forma quase total, a jovens em propagandas de produtos cada vez mais novos. Reforça-se, assim, a ideologia de que o novo é o bom; já o velho deve ser descartado. Diante disso, há, cada vez mais, um distanciamento nas relações interpessoais entre idosos e jovens, pois estes vêem aqueles como símbolo do retrocesso, objetos descartados.
+# Dessa maneira, fica perceptível que o respeito ao idoso está longe do que deveria ser, pois ainda é preciso quebrar paradigmas e imposições que levam ao subjugamento de pessoas. Nessa perspectiva, é preciso que ONG´s, juntamente, com a mídia, realizem campanhas que valorizem o idoso, apontando qualidades como a experiência e a contribuição dada para a estruturação da sociedade. Ainda, a escola e a família devem preparar crianças para terem respeito às diferenças e para se conscientizarem do processo biológico do envelhecimento, através de visitas periódicas a casas
+# de acolhimento de idosos. Cabe ainda à família planejar o conforto e a ambientação do idoso por meio de adaptações em imóveis, móveis e transportes.’’’
+
+# """
+
+@app.route('/corrigir_redacao', methods=['POST'])
+def get_completion(model="gpt-3.5-turbo"):
+
+    data = request.get_json()
+
+    redacao = data['redacao']
+
+    messages = [
+        {"role": "system", "content": prompt()},
+        {"role": "user", "content": redacao},
     ]
-
     response = openai.ChatCompletion.create(
-        model=modelo,  # Use o modelo desejado, por exemplo, "gpt-3.5-turbo"
-        messages=conversa
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
     )
+    return jsonify({"correcao": response.choices[0].message["content"]})
 
-    return response['choices'][0]['message']['content']
+# response = get_completion(prompt())
+# print(response)
 
-# @app.route('/corrigir_redacao', methods=['POST'])
-def corrigir_redacao_endpoint(redacao):
-    # dados = request.get_json()
-    # redacao = dados.get('redacao')
-
-    if redacao:
-        modelo = 'gpt-3.5-turbo'  # Substitua pelo modelo desejado
-        correcao = criar_conversa(redacao, modelo)
-        # return jsonify({"correcao": correcao})
-    # else:
-        # return jsonify({"erro": "Redação não fornecida"})
-
-# if __name__ == "__main":
-#     app.run()
-
-print(corrigir_redacao_endpoint("A imposição de padrões culturais e a obsessão por verdades absolutas impedem religiosos conservadores de conviver com as diferenças, o que acaba gerando a intolerância religiosa. Isso ocorre devido à falta do sentimento de coexistência, por parte dos intolerantes, com a fé do outro. Esse problema está enraizado na nossa história, um exemplo disso são os constantes ataques às religiões afro-brasileiras, nos quais os próprios agressores julgam a fé alheia sem mesmo conhecê-la ou até mesmo na imposição da religião cristã aos antigos nativos que aqui habitavam. Segundo Edgar Morin, mudanças de pensamento implicam, antes de tudo, em mudanças na educação, ou seja, teremos que mudar a nossa educação de individual e tecnicista, para uma mais humana e coletiva. Além disso, para revertermos esse quadro, a laicidade do Estado deveria ser assegurada e políticas de opressão a esse tipo de agressão deveriam ser tomadas, como por exemplo, o aumento da pena para quem cometer esses crimes e uma maior campanha de denúncia. A fim de assegurar o avanço contra essas práticas, uma medida à médio-longo prazo, seria a implementação da disciplina Ensino religioso nas escolas brasileiras. Essa disciplina, testada em escolas suecas, consiste em mostrar de maneira imparcial religiões de todos os continentes para que a visão de mundo do aluno seja ampliada desde sua infância, o que pode ajudar, de maneira direta, à redução desse problema"))
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
---
+++ backend/prompt.py
@@ -0,0 +1,111 @@
+def competencias():
+    competencias = """
+    As competências do Enem são:
+    1. Domínio da escrita formal da língua portuguesa: é avaliado se a redação do participante está adequada às regras de ortografia, como acentuação, ortografia, uso de hífen, emprego de letras maiúsculas e minúsculas e separação silábica.
+2. Compreender o tema e não fugir do que é proposto - avalia as habilidades integradas de leitura e de escrita do candidato.
+3. Selecionar, relacionar, organizar e interpretar informações, fatos, opiniões e argumentos em defesa de um ponto de vista
+O candidato precisa elaborar um texto que apresente, claramente, uma ideia a ser defendida e os argumentos que justifiquem a posição assumida em relação à temática da proposta da redação.
+4. Conhecimento dos mecanismos linguísticos necessários para a construção da argumentação
+São avaliados itens relacionados à estruturação lógica e formal entre as partes da redação. A organização textual exige que as frases e os parágrafos estabeleçam entre si uma relação que garanta uma sequência coerente do texto e a interdependência entre as ideias.
+5. Respeito aos direitos humanos
+Apresentar uma proposta de intervenção para o problema abordado que respeite os direitos humanos. Propor uma intervenção para o problema apresentado pelo tema significa sugerir uma iniciativa que busque, mesmo que minimamente, enfrentá-lo.
+Para correção da redação siga a seguinte Estratégia
+"""
+    return competencias
+
+def estrategia():
+    estrategia = """I) Enxergar o problema – linha argumentativa
+II) Relacionar duas causas para o problema / um exemplo / duas consequências / fala de auto-
+ridade.
+III) Selecionar agentes envolvidos."""
+    return estrategia
+
+def linha1():
+    linha_arg_1 = """Linha Argumentativa 1 - Relações de poder
+
+Estratégias
+A. Identificar PADRÕES IMPOSTOS pelo grupo considerado OPRESSOR.
+B. Sugerir FORMAÇÃO DAS RELAÇÕES: Cultural e historicamente, grupos inferiorizam outros
+com o objetivo de obtenção de vantagens individuais / manutenção de posição social ou
+econômica.
+C. Relacionar os padrões impostos à obsessão por determinadas verdades.
+D. Elencar DERIVAÇÕES do modelo: Pretensa superioridade; visão maniqueísta; agressões...
+E. Selecionar AGENTES ENVOLVIDOS:
+i) ESCOLA: tornar o ensino ético
+ii) GOVERNO: criar instrumentos que facilitem a aplicação das leis, quando houver abuso.
+
+TEMAS
+Os temas que podem ser desenvolvidos de acordo com essa linha envolvem rela-
+ções de poder entre diferentes: mulher; negro; não-cristão; indígena; idoso; criança/adolescente;
+nordestino; homossexual; pobre; gordo; deficiente; eleitor; operário; espectador; consumidor;
+imigrante. """
+    return linha_arg_1
+
+def linha2():
+    linha_arg_2 = """Linha argumentativa 2 - cidadania e participação social
+
+Os textos que podem ser desenvolvidos de acordo com essa linha estão relacionados
+por, neles, haver uma “sugestão” de que, limitados por interesses individuais e baseados numa
+cultura de transgressão e de privilégios, os seres humanos costumam lesar o bem comum.
+Vamos adentrar agora em uma linha de argumentação que discutirá:
+1- O que nos impede de lutar por um projeto coletivo mais amplo, visto que somos tão indignados com os problemas do país?
+2- Por que não nos incomodamos quando são usurpados direitos alheios?
+3- Por que não nos incomoda o sofrimento do outro?
+
+Estratégias:
+A. Identificar o que está impedindo o EXERCÍCIO DA CIDADANIA.
+B. Sugerir como a falta de cidadania contribui para a MANUTENÇÃO DA CULTURA DE PRIVILÉGIOS, DE TRANSGRESSÃO E DE EXPLORAÇÃO.
+C. Apontar as consequências dessa cultura: PERPETUAÇÃO DAS RELAÇÕES DE PODER,
+BENEFICIAMENTO A GRUPOS ESPECÍFICOS, INDIFERENÇA À SITUAÇÃO ALHEIA, UTILIZAÇÃO DO OUTRO COMO MECANISMO DE CONQUISTAS INDIVIDUAIS, ACOMODAÇÃO, PRAZER EM QUEBRAR REGRAS.
+D. Selecionar AGENTES ENVOLVIDOS."""
+    return linha_arg_2
+
+def linha3():
+    linha_arg_3 = """Linha argumentativa 3 - efeitos de consumismo sobre os valores humanos
+
+Estratégias:
+Tema: Consumismo e felicidade
+
+A) Identificar AS CARACTERÍSTICAS DA RELAÇÃO COM OS OBJETOS que foram TRANSFERIDAS PARA AS RELAÇÕES HUMANAS – A COISIFICAÇÃO dos seres humanos.
+B) Relacionar a INTENSIFICAÇÃO DAS RELAÇÕES COM OS OBJETOS e a MINIMIZAÇÃO
+DE LAÇOS HUMANOS SÓLIDOS.
+C) Acrescentar, SE FOR CONVENIENTE, a mídia como ferramenta de alienação e manipulação.
+D) Selecionar AGENTES ENVOLVIDOS:
+I) ESCOLA: tornar o ensino ético, voltado para valores coletivos.
+II) GOVERNO: criar instrumentos que minimizem o comportamento consumista / estimular linhas de crédito que visem investimentos em cursos que priorizem uma educação voltada para a formação cidadã.
+
+"""
+    return linha_arg_3
+
+def exemplo():
+    exemplo = """
+Tema: Caminhos para combater a intolerância religiosa no Brasil
+
+A imposição de padrões culturais e a obsessão por verdades absolutas impedem religiosos conservadores de conviver com as diferenças, o que acaba gerando a intolerância religiosa. Isso ocorre devido à falta do sentimento de coexistência, por parte dos intolerantes, com a fé do outro. Esse problema está enraizado na nossa história, um exemplo disso são os constantes ataques às religiões afro-brasileiras, nos quais os próprios agressores julgam a fé alheia sem mesmo conhecê-la ou até mesmo na imposição da religião cristã aos antigos nativos que aqui habitavam.
+Segundo Edgar Morin, mudanças de pensamento implicam, antes de tudo, em mudanças na educação, ou seja, teremos que mudar a nossa educação de individual e tecnicista, para uma mais humana e coletiva. Além disso, para revertermos esse quadro, a laicidade do Estado deveria ser assegurada e políticas de opressão a esse tipo de agressão deveriam ser tomadas, como por exemplo, o aumento da pena para quem cometer esses crimes e uma maior campanha de denúncia.
+A fim de assegurar o avanço contra essas práticas, uma medida a médio-longo prazo, seria a implementação da disciplina Ensino religioso nas escolas brasileiras. Essa disciplina, testada em escolas suecas, consiste em mostrar de maneira imparcial religiões de todos os continentes para que a visão de mundo do aluno seja ampliada desde sua infância, o que pode ajudar, de maneira direta, à redução desse problema.
+
+Comentário geral sobre o texto:
+Facilmente, percebemos que o texto acima tenta seguir nossa metodologia, visto que se baseia em uma das linhas argumentativas, já conhecida do aluno. No entanto, ao longo do texto, não percebemos o desenvolvimento dessa linha. A “falha” dessa redação consiste no fato de não desenvolver aquilo a que se propôs, visto que a ideia estruturante (a relação de poder) foi apenas exposta na introdução. Ainda, a problemática proposta – que é relacionada à falta de sentimento de aceitação da coexistência e às relações históricas – não é discutida, na medida em que o único parágrafo de desenvolvimento se direciona à apresentação de novas problemáticas – relacionadas à educação e à laicidade do Estado – sem que elas tenham relação com a introdução do texto. Além disso, a proposta de intervenção, presente no último parágrafo, também não possui muita ligação com a discussão que vinha sendo desenvolvida, pois apenas sugere uma relação do tema com a questão educacional. Apresenta, assim, uma proposta de intervenção (solução) que põe em cena um agente (a escola) que não foi incluído na problematização do tema (combate à intolerância religiosa). Os outros problemas apresentados no texto – a fragilidade da laicidade do Estado, a ausente construção de um sentimento de aceitação da coexistência e as raízes históricas da intolerância – não possuem proposta de intervenção para resolvê-los.
+
+Orientações para a reescrita:
+Quanto à introdução: procure estabelecer uma problemática que possa ser trabalhada nos outros parágrafos, por exemplo, desenvolva a problemática relacionando à educação, tendo em vista que você vai utilizá-la para sua argumentação; aponte como você vê a realidade da escola em relação ao tema; parta da percepção da sua escola mesmo; se for discutir a laicidade do Estado, é interessante que a apresente na introdução; perceba que utilizar expressões como “está enraizado na nossa história” – sem desenvolver questões históricas posteriormente - e “um exemplo” não agregam força argumentativa à introdução.
+Quanto ao desenvolvimento: o primeiro parágrafo já se inicia encaminhando para uma proposta de intervenção, o que é mais comum na conclusão do texto – “teremos que mudar a nossa educação individual e tecnicista...” / “para revertermos esse quadro...”–. Procure, primeiramente, discutir os problemas apresentados na introdução. Você pode iniciar o desenvolvimento retomando o problema da ausência do sentimento de coexistência e o associando à questão educacional. É interessante também acrescentar exemplos, dados ou fatos a fim de comprovar
+o que se afirma sobre a educação. Uma maneira convincente de fazer isso é utilizar expressões como “PROVA CLARA DISSO” / “CONSEQUÊNCIA DIRETA DESSE...”, afinal, elementos coesivos dessa natureza funcionam como operadores argumentativos que contribuem para a argumentação. Assim, teríamos “prova clara dessa pouca capacidade das escolas é”. Além disso, sempre que for acrescentar algum exemplo, fato ou dado à sua argumentação, reflita de que maneira você pode relacionar os novos elementos de forma que eles reforcem ainda mais a ideia que você defende. &&&
+Para dar continuidade ao desenvolvimento, escreva um novo parágrafo a fim de discutir a laicidade do Estado. Dessa forma, seguindo uma estruturação próxima àquela sugerida para o primeiro parágrafo de desenvolvimento, o texto conseguiria discutir, por completo, a problemática apresentada. Assim, com os necessários ajustes, a proposta de intervenção estaria melhor relacionada às outras partes do texto.
+
+"""
+    return exemplo
+
+def prompt():
+    prompt = f"""Você é corretor de dissertações e seu papel envolve fornecer feedback e direcionamento para melhorar redações argumentativas.\
+Uma dissertação-argumentativa requer mais do que apenas abordar o tema; é essencial entender e relacionar o tema a informações amplas, \
+caracterizando diferentes linhas de argumentação. A escolha da linha de argumentação é crucial, pois define a estratégia de planejamento e \
+construção do texto. \
+Para elaborar o feedback você deve utilizar as competências do Enem ```{competencias()}```, a estratégia de construção do texto dissertativo-argumentativo \
+```{estrategia()}``` e as linhas argumentativas ```{linha1()}``, ```{linha2()}``, ```{linha3()}```.
+
+Você receberá um texto de exemplo de feedback delimitado por parênteses (```{exemplo()}```)
+
+"""
+    return prompt
